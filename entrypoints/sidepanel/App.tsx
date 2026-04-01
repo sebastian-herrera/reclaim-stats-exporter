0e2c27b8a65d1e4435e694775a0b3e2e62950aca
Sebastian Herrera <herrera6911sebastian@hotmail.com>
2026-03-31 22:23:37 -0500

    feat: sidebar

---
+++ entrypoints/sidepanel/App.tsx
@@ -0,0 +1,16 @@
+import '@/assets/tailwind.css';
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <Button variant="secondary">asd</Button>
+      <p className="text-red-500">
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+      <div>x</div>
+    </>
+  );
+}
+
+export default App;
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,20 @@
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  vite: () => ({
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
+      },
+    },
+  }),
+  webExt: {
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+    disabled: true,
+  },
 });
